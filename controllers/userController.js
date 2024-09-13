d2474bc851390d2e5820e1c94f8f3db341d17a4e
Kunj Tyagi <kunjtyagi24@gmail.com>
2024-09-14 00:58:49 +0530

    Complete-untill-User-Login

---
+++ controllers/userController.js
@@ -0,0 +1,54 @@
+const asyncHandler = require("express-async-handler");
+const User=require("../models/userModels");
+const bcrypt=require("bcryptjs");
+
+// @desc Register a user 
+// @routes POST /api/users/register
+// @access public
+const registerUser = asyncHandler(async(req,res)=>{
+    let {username,email,password}=req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All field are mandatory");
+    }
+    const userAvailable=await User.findOne({email});
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already registered!");
+    }
+
+    // Hash Password // 10-salt round
+    const hashedPassword=await bcrypt.hash(password,10);
+    console.log("Hashed Password: ",hashedPassword);
+    const user=await User.create({
+        username,
+        email,
+        password:hashedPassword,
+    })
+
+    console.log(`User created ${user}`);
+    if(user){
+        res.status(201).json({_id:user.id,email:user.email})
+    }
+    else{
+        res.status(400);
+        throw new Error("User is not valid");
+    }
+    res.json({message:"Register the user!"});
+});
+
+// @desc Login a user 
+// @routes POST /api/users/login
+// @access public
+const loginUser = asyncHandler(async(req,res)=>{
+    res.json({message:"login user!"});
+});
+
+// @desc current user info
+// @routes POST /api/users/current
+// @access Private
+const currentUser = asyncHandler(async(req,res)=>{
+    res.json({message:"Current user information!"});
+});
+
+module.exports={registerUser,loginUser,currentUser};
---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const errorhandler = require("./middleware/errorhandler");
+const connectdb = require("./config/dbConnection");
+const dotenv=require("dotenv").config();
+const app=express();
+
+const port=process.env.PORT;
+
+connectdb();
+
+app.use(express.json());
+app.use("/api/contacts",require("./routes/contactRoutes"));
+app.use("/api/users",require("./routes/userRoutes"));
+app.use(errorhandler);
+
+app.listen(port,()=>{
+    console.log(`Server is started ${port}`);
+})
